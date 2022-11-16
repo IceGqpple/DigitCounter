3dad5cb1fd2b78fd3f396f9a467710342ef429c7
IceGqpple <arne.alexander05@gmail.com>
2022-11-16 14:20:11 +0100

    Add project files.

---
+++ DigitCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter();
+        }
+
+        public static void Counter()
+        {
+            int Num = 3256;
+            int digits = Num.ToString().Length;
+            Console.WriteLine(digits);
+            Console.ReadLine();
+        }
+    }
+}
